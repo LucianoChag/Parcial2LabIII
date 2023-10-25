505b8cd75bc04c824965a4385830c2ed523d40c4
Luciano Chagnaud <128649774+LucianoChag@users.noreply.github.com>
2023-10-25 00:10:48 -0300

    Función para resolver la ecuación agregada
    Se implementó las funciones que resuelven la ecuación. Todavía hay que corregir unos detalles.

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -1,13 +1,14 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
 
 class Ecuacion
 {
     // Función para validar que la ecuación no comience ni termine con algún operador
     static void ValidarOperadoresOperandos(string input)
     {
-        List<char> operandos = new List<char>{
-            '+', '-', '/', '*', '.'
-        };
+        List<char> operandos = new List<char> { '+', '-', '/', '*', '.' };
 
         char primerCaracter = input[0];
         char ultimoCaracter = input[input.Length - 1];
@@ -16,7 +17,9 @@ class Ecuacion
         {
             if (primerCaracter != '-')
             {
-                throw new InvalidOperationException("El primer y último carácter de la ecuación no pueden ser operadores o puntos decimales.");
+                throw new InvalidOperationException(
+                    "El primer y último carácter de la ecuación no pueden ser operadores o puntos decimales."
+                );
             }
         }
     }
@@ -34,7 +37,9 @@ class Ecuacion
             {
                 if (puntoDecimalEnCurso)
                 {
-                    throw new InvalidOperationException("Dos puntos decimales consecutivos no están permitidos.");
+                    throw new InvalidOperationException(
+                        "Dos puntos decimales consecutivos no están permitidos."
+                    );
                 }
                 puntoDecimalEnCurso = true;
             }
@@ -45,7 +50,9 @@ class Ecuacion
 
             if (i == input.Length - 1 && caracterActual == '.')
             {
-                throw new InvalidOperationException("El número no puede terminar con un punto decimal.");
+                throw new InvalidOperationException(
+                    "El número no puede terminar con un punto decimal."
+                );
             }
         }
     }
@@ -65,11 +72,15 @@ class Ecuacion
                 contadorParentesis++;
                 if (caracSig == '+' || caracSig == '*' || caracSig == '/' || caracSig == '.')
                 {
-                    throw new InvalidOperationException("Operador no válido después de abrir un paréntesis.");
+                    throw new InvalidOperationException(
+                        "Operador no válido después de abrir un paréntesis."
+                    );
                 }
                 if (caracAnterior == '.')
                 {
-                    throw new InvalidOperationException("Previo a un paréntesis no puede haber un '.'.");
+                    throw new InvalidOperationException(
+                        "Previo a un paréntesis no puede haber un '.'."
+                    );
                 }
             }
             else if (caracterActual == ')')
@@ -79,9 +90,17 @@ class Ecuacion
                 {
                     throw new InvalidOperationException("Los paréntesis no están balanceados.");
                 }
-                if (caracAnterior == '+' || caracAnterior == '-' || caracAnterior == '*' || caracAnterior == '/' || caracAnterior == '.')
+                if (
+                    caracAnterior == '+'
+                    || caracAnterior == '-'
+                    || caracAnterior == '*'
+                    || caracAnterior == '/'
+                    || caracAnterior == '.'
+                )
                 {
-                    throw new InvalidOperationException("Operador no válido antes de cerrar un paréntesis.");
+                    throw new InvalidOperationException(
+                        "Operador no válido antes de cerrar un paréntesis."
+                    );
                 }
             }
         }
@@ -117,8 +136,25 @@ class Ecuacion
     // Función para verificar que no haya caracteres inválidos
     static void ValidarCaracteres(string input)
     {
-        List<char> caracteresValidos = new List<char>{
-            '+', '-', '/', '*', '(', ')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'
+        List<char> caracteresValidos = new List<char>
+        {
+            '+',
+            '-',
+            '/',
+            '*',
+            '(',
+            ')',
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '.'
         };
 
         foreach (char caracter in input)
@@ -129,13 +165,25 @@ class Ecuacion
                 switch (auxChar)
                 {
                     case ',':
-                        throw new InvalidOperationException("Utilizó un carácter inválido: '" + caracter + "'. Prefiera el uso de '.' en lugar de este carácter.");
+                        throw new InvalidOperationException(
+                            "Utilizó un carácter inválido: '"
+                                + caracter
+                                + "'. Prefiera el uso de '.' en lugar de este carácter."
+                        );
                     case ' ':
-                        throw new InvalidOperationException("Utilizó un carácter inválido: ' '. Escriba la ecuación sin espacios.");
+                        throw new InvalidOperationException(
+                            "Utilizó un carácter inválido: ' '. Escriba la ecuación sin espacios."
+                        );
                     case 'x':
-                        throw new InvalidOperationException("Utilizó un carácter inválido: '" + caracter + "'. Prefiera el uso de '*' en lugar de este carácter.");
+                        throw new InvalidOperationException(
+                            "Utilizó un carácter inválido: '"
+                                + caracter
+                                + "'. Prefiera el uso de '*' en lugar de este carácter."
+                        );
                     default:
-                        throw new InvalidOperationException("Utilizó un carácter inválido: " + caracter);
+                        throw new InvalidOperationException(
+                            "Utilizó un carácter inválido: " + caracter
+                        );
                 }
             }
         }
@@ -158,45 +206,47 @@ class Ecuacion
         }
     }
 
-static void ValidarOperandoMinimo(string input)
-{
-    List<char> operandos = new List<char> { '+', '-', '*', '/' };
-    bool contieneOperando = false;
+    static void ValidarOperandoMinimo(string input)
+    {
+        List<char> operandos = new List<char> { '+', '-', '*', '/' };
+        bool contieneOperando = false;
 
-    if (input[0] == '-')
-    {
-        for (int i = 1; i < input.Length; i++)
-        {  
-            char caracter = input[i];
-            if (operandos.Contains(caracter))
-            {
-            contieneOperando = true;
-            break;
-            }   
-        }
-    }
-    else
-    {
-        foreach (char caracter in input)
+        if (input[0] == '-')
         {
-            if (operandos.Contains(caracter))
+            for (int i = 1; i < input.Length; i++)
             {
-            contieneOperando = true;
-            break;
+                char caracter = input[i];
+                if (operandos.Contains(caracter))
+                {
+                    contieneOperando = true;
+                    break;
+                }
             }
         }
+        else
+        {
+            foreach (char caracter in input)
+            {
+                if (operandos.Contains(caracter))
+                {
+                    contieneOperando = true;
+                    break;
+                }
+            }
+        }
+
+        if (!contieneOperando)
+        {
+            throw new InvalidOperationException(
+                "Es necesario que se realice MINIMO una operación."
+            );
+        }
     }
 
-    if (!contieneOperando)
+    //Función que divide la expresión matemática en tokens y la almacena en una lista
+    static List<object> TokenizarExpresion(string expresion)
     {
-        throw new InvalidOperationException("Es necesario que se realice MINIMO una operación.");
-    }
-}
-
-static List<object> TokenizarExpresion(string expresion)
-//Tokeniza una expresión matemática en una lista de objetos que representan los elementos de la expresión.
-// Los números se convierten en valores double, y los operadores y otros caracteres se mantienen como cadenas.
-    {
+        // Lista para almacenar los tokens resultantes
         List<object> tokens = new List<object>();
         string token = "";
 
@@ -204,18 +254,22 @@ static List<object> TokenizarExpresion(string expresion)
         {
             char caracter = expresion[i];
 
+            // Ignorar espacios en blanco
             if (char.IsWhiteSpace(caracter))
                 continue;
 
             if (char.IsDigit(caracter) || caracter == '.')
             {
+                // Acumular caracteres numéricos para formar un número
                 token += caracter;
             }
             else
             {
                 if (!string.IsNullOrEmpty(token))
                 {
-                    if (TryParseDouble(token, out double numero))
+                    // Si se encontró un operador o paréntesis, 
+                    // convertir el token previo (si es un número) a double y agregarlo a la lista de tokens
+                    if (double.TryParse(token, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double numero))
                     {
                         tokens.Add(numero);
                     }
@@ -223,15 +277,27 @@ static List<object> TokenizarExpresion(string expresion)
                     {
                         throw new FormatException("Error al convertir el token a número: " + token);
                     }
-                    token = "";
+                    token = ""; // Reiniciar el token
                 }
+
+                if (caracter == '(')
+                {
+                    // Insertar un operador de multiplicación implícito si hay un número antes del paréntesis
+                    if (tokens.Count > 0 && tokens[tokens.Count - 1] is double)
+                    {
+                        tokens.Add("*");
+                    }
+                }
+
+                // Agregar el operador o paréntesis a la lista de tokens
                 tokens.Add(caracter.ToString());
             }
         }
 
         if (!string.IsNullOrEmpty(token))
         {
-            if (TryParseDouble(token, out double numero))
+            // Procesar cualquier número que quede al final de la expresión
+            if (double.TryParse(token, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double numero))
             {
                 tokens.Add(numero);
             }
@@ -244,17 +310,132 @@ static List<object> TokenizarExpresion(string expresion)
         return tokens;
     }
 
-    static bool TryParseDouble(string s, out double result)
+    //Función que realiza las operaciones necesarias para llegar al resultado final invocando a las demás funciones
+    static double RealizarOperacion(List<object> tokens)
+{
+    Stack<double> numeros = new Stack<double>();
+    Stack<string> operadores = new Stack<string>();
+
+    foreach (object token in tokens)
     {
-        return double.TryParse(s, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out result);
+        if (token is double)
+        {
+            // Si el token es un número, lo apilamos en la pila de números.
+            numeros.Push((double)token);
+        }
+        else if (token is string)
+        {
+            string operador = (string)token;
+            if (operador == "(")
+            {
+                // Si encontramos un paréntesis abierto, lo apilamos en la pila de operadores.
+                operadores.Push(operador);
+            }
+            else if (operador == ")")
+            {
+                // Si encontramos un paréntesis cerrado, calculamos las operaciones dentro del paréntesis.
+                while (operadores.Count > 0 && operadores.Peek() != "(")
+                {
+                    RealizarCalculo(numeros, operadores);
+                }
+                if (operadores.Count > 0 && operadores.Peek() == "(")
+                {
+                    operadores.Pop(); // Elimina el paréntesis abierto
+                }
+            }
+            else if (EsOperadorValido(operador))
+            {
+                // Si encontramos un operador válido, verificamos su prioridad y realizamos cálculos si es necesario.
+                while (operadores.Count > 0 && PrioridadOperador(operador) <= PrioridadOperador(operadores.Peek()))
+                {
+                    RealizarCalculo(numeros, operadores);
+                }
+                operadores.Push(operador);
+            }
+            else
+            {
+                throw new ArgumentException("Operador no válido: " + operador);
+            }
+        }
     }
+
+    // Realizamos cualquier cálculo restante.
+    while (operadores.Count > 0)
+    {
+        RealizarCalculo(numeros, operadores);
+    }
+
+    // Al final, la pila de números debe contener el resultado final.
+    if (numeros.Count != 1)
+    {
+        throw new InvalidOperationException("La expresión no es válida.");
+    }
+
+    return numeros.Pop();
+}
+
+static bool EsOperadorValido(string operador)
+{
+    // Verificamos si el operador es uno de los operadores válidos: +, -, *, /
+    return operador == "+" || operador == "-" || operador == "*" || operador == "/";
+}
+
+static int PrioridadOperador(string operador)
+{
+    // Asignamos prioridades a los operadores, donde * y / tienen mayor prioridad que + y -.
+    switch (operador)
+    {
+        case "+":
+        case "-":
+            return 1;
+        case "*":
+        case "/":
+            return 2;
+        default:
+            return 0; // Operador no válido
+    }
+}
+
+static void RealizarCalculo(Stack<double> numeros, Stack<string> operadores)
+{
+    // Realizamos una operación con los dos últimos números y el último operador.
+    double operando2 = numeros.Pop();
+    double operando1 = numeros.Pop();
+    string operador = operadores.Pop();
+
+    double resultado = 0;
+
+    // Realizamos la operación adecuada según el operador.
+    switch (operador)
+    {
+        case "+":
+            resultado = operando1 + operando2;
+            break;
+        case "-":
+            resultado = operando1 - operando2;
+            break;
+        case "*":
+            resultado = operando1 * operando2;
+            break;
+        case "/":
+            if (operando2 == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero.");
+            }
+            resultado = operando1 / operando2;
+            break;
+    }
+
+    // Apilamos el resultado nuevamente en la pila de números.
+    numeros.Push(resultado);
+}
 
 
     // MAIN
     static void Main(string[] args)
     {
         // INPUT INGRESADO POR EL USUARIO
-        string input = "2-33(4.5+9)/2";
+        string input = "3/0.2"; //No resuelve resultados que comienzan negativos. Si termina con parentesis vacios no lo resuelve. Resolver tema del rango en división que finaliza con 0
 
         try
         {
@@ -279,27 +460,20 @@ static List<object> TokenizarExpresion(string expresion)
             // Verificamos que haya al menos un operando
             ValidarOperandoMinimo(input);
 
-        List<object> tokens = TokenizarExpresion(input);
+            Console.WriteLine("Ecuación: " + input);
 
-        foreach (var token in tokens)
-        {
-            Console.WriteLine(token);
-        }
+            List<object> tokens = TokenizarExpresion(input);
 
-            Console.WriteLine(input);
+            double resultado = RealizarOperacion(tokens);
+            Console.WriteLine("Resultado: " + resultado);
         }
         catch (InvalidOperationException ex)
         {
             Console.WriteLine(ex.Message);
         }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
-
-
-
-
-
-
-
-
-//Entrada de datos - Proceso - Salida
